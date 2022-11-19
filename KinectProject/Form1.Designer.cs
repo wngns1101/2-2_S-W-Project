c749ad514d97dcf5d2e741bde0dd2a09a1d7e843
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 <wngns0812@gmail.com>
2022-11-19 20:21:46 +0900

    add login and height function

---
+++ KinectProject/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace KinectProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.pw = new System.Windows.Forms.TextBox();
+            this.SingInButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(216, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "로그인";
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.id.Location = new System.Drawing.Point(163, 117);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(200, 32);
+            this.id.TabIndex = 1;
+            this.id.Text = "아이디";
+            // 
+            // pw
+            // 
+            this.pw.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.pw.Location = new System.Drawing.Point(163, 170);
+            this.pw.Name = "pw";
+            this.pw.Size = new System.Drawing.Size(200, 32);
+            this.pw.TabIndex = 2;
+            this.pw.Text = "비밀번호";
+            // 
+            // SingInButton
+            // 
+            this.SingInButton.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.SingInButton.Location = new System.Drawing.Point(163, 235);
+            this.SingInButton.Name = "SingInButton";
+            this.SingInButton.Size = new System.Drawing.Size(200, 37);
+            this.SingInButton.TabIndex = 3;
+            this.SingInButton.Text = "로그인";
+            this.SingInButton.UseVisualStyleBackColor = true;
+            this.SingInButton.Click += new System.EventHandler(this.SingInButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 420);
+            this.Controls.Add(this.SingInButton);
+            this.Controls.Add(this.pw);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox pw;
+        private System.Windows.Forms.Button SingInButton;
+    }
+}
+
---
+++ KinectProject/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinectProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SingInButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnection = "datasource = localhost; port=3306; username=root; password=rootpw; database = c#db";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+
+                MySqlCommand SelectCommand = new MySqlCommand("select * from user " + "where id = '"+this.id.Text + "' and pw = '"+this.pw.Text + "';", myConn);
+
+                MySqlDataReader myReader;
+                myConn.Open();
+                myReader = SelectCommand.ExecuteReader();
+                int count = 0;
+
+                while (myReader.Read())
+                {
+                    count = count + 1;
+                }
+
+                if(count == 1)
+                {
+                    MessageBox.Show("안녕세요, 관리자님");
+                    MainWindow height = new MainWindow();
+                }
+                else
+                {
+                    MessageBox.Show("아이디와 패스워드가 불일치합니다.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
